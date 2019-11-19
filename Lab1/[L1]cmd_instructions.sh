d621399f2d55789123f671af7754ef9a579ec07c
Iason Georgios Velentzas <34183791+JasonVel6@users.noreply.github.com>
2019-11-19 23:57:26 +0200

    Changing Format of File

--- Lab1/[L1]cmd_instructions.sh
+++ Lab1/[L1]cmd_instructions.sh
@@ -7,7 +7,8 @@
 ./build/ARM/gem5.opt -d MinorCPU_orig configs/example/se.py --cpu-type=MinorCPU --caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
 
 #TimingSimpleCPU
-./build/ARM/gem5.opt -d TimingSimpleCPU_orig configs/example/se.py --cpu-type=TimingSimpleCPU --caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
+./build/ARM/gem5.opt -d TimingSimpleCPU_orig configs/example/se.py --cpu-type=TimingSimpleCPU 
+--caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
 
 
 #1.3.c Changed CPU_clock and Mem_type in both cases:
@@ -15,15 +16,19 @@
 ###CPU_clock
 
 #MinorCPU
-./build/ARM/gem5.opt -d MinorCPU_orig/CPUCL3GHz configs/example/se.py --cpu-type=MinorCPU --cpu-clock=3GHz --caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
+./build/ARM/gem5.opt -d MinorCPU_orig/CPUCL3GHz configs/example/se.py --cpu-type=MinorCPU --cpu-clock=3GHz 
+--caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
 
 #TimingSimpleCPU
-./build/ARM/gem5.opt -d TimingSimpleCPU_orig/CPUCL3GHz configs/example/se.py --cpu-type=TimingSimpleCPU --cpu-clock=3GHz --caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
+./build/ARM/gem5.opt -d TimingSimpleCPU_orig/CPUCL3GHz configs/example/se.py --cpu-type=TimingSimpleCPU --cpu-clock=3GHz 
+--caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
 
 ###Mem_type
 
 #MinorCPU
-./build/ARM/gem5.opt -d MinorCPU_orig/MEM_DDR4_2400_16x4 configs/example/se.py --cpu-type=MinorCPU --mem-type=DDR4_2400_16x4 --caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
+./build/ARM/gem5.opt -d MinorCPU_orig/MEM_DDR4_2400_16x4 configs/example/se.py --cpu-type=MinorCPU --mem-type=DDR4_2400_16x4 
+--caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
 
 #TimingSimpleCPU
-./build/ARM/gem5.opt -d TimingSimpleCPU_orig/MEM_DDR4_2400_16x4 configs/example/se.py --cpu-type=TimingSimpleCPU --mem-type=DDR4_2400_16x4 --caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
+./build/ARM/gem5.opt -d TimingSimpleCPU_orig/MEM_DDR4_2400_16x4 configs/example/se.py --cpu-type=TimingSimpleCPU --mem-type=DDR4_2400_16x4
+--caches -c ~/gem5-CA-Lab-Auth-/Lab1/Part3/test_arm
